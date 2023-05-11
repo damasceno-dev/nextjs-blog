1a1423d07761acfcbecf8ac69e76253c61f515b3
Fast Ninja <damasceno.developer21@gmail.com>
2023-05-11 00:49:10 -0300

    before get statics props

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,11 @@
 import Head from 'next/head';
 import Layout, { siteTitle } from '../components/layout';
+import { getSortedPostsData } from '../lib/posts';
 import utilStyles from '../styles/utils.module.css';
 
 export default function Home() {
+  const posts = getSortedPostsData()
+  console.log(posts)
   return (
     <Layout home>
       <Head>
